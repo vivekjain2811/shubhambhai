64867a16c2ad5b73f7f6c4318d42eeb478e70301
vivek jain <vivekjain.vj.2019@gmail.com>
2024-04-25 09:49:15 +0530

    final-done

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import reviews from "./Components/data";
 
 function App() {
   return (
-  <div className="flex flex-col w-[100vw] h-[100vh] justify-center items-center bg-gray-200">
+  <div className="flex flex-col w-[100vw] h-[100vh]  justify-center items-center bg-gray-200">
 <div className="text-center ">
 <h1 className="text-4xl font-bold"> Our Testinomial</h1>
-<div className="bg-violet-400 h-[4px] w-[1/5]"></div>
+<div className="bg-violet-400 h-[4px] w-1/5 mx-auto mt-1"></div>
 <Testinomial reviews={reviews}></Testinomial>
 
  
--- src/Components/Testinomial.js
+++ src/Components/Testinomial.js
@@ -1,21 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./Card";
 import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 function Testinomial(props) {
     let reviews=props.reviews;
-    return(
-    <div>
-<Card review={reviews[0]}></Card>
+    const [index,setINndex]=useState(0);
+
+   function leftshiftHandler(){
+if(index-1<0){
+setINndex(reviews.lemgth-1);
+}
+else
+{
+  setINndex(index-1);
+}
+   }
+function rightshiftHandler(){
+if(index+1>=reviews.length)
+{
+  setINndex(0);
+}
+else{
+  setINndex(index+1);
+}
+}
+function surpriseHandler(){
+let randomIndex=Math.floor(Math.random()*reviews.length);
+setINndex(randomIndex);
+}
+return(
+    <div className="w-[85vw] md:w-[700px] bg-white flex flex-col justify-center items-center mt-10 p-10 transition-all duration-700 hover:shadow-xl ">
+<Card review={reviews[index]}></Card>
 <div className="flex text-3xl mt-5 gap-3 text-violet-400 font-bold mx-auto">
-        <button className="cursor-pointer  hover:text-violet-500">
+        <button onClick={leftshiftHandler} className="cursor-pointer  hover:text-violet-500">
           <FiChevronLeft></FiChevronLeft>
         </button>
-        <button className="cursor-pointer  hover:text-violet-500">
+        <button onClick={rightshiftHandler} className="cursor-pointer  hover:text-violet-500">
           <FiChevronRight></FiChevronRight>
         </button>
       </div>
       <div>
-        <button
+        <button onClick={surpriseHandler}
           className="bg-violet-400 hover:bg-violet-500 transition-all 
         duration-200 cursor-pointer px-10 py-2 rounded-md font -bold text-white text-lg"
         >
